af28dbadcc103ea808373d2f17420166442d832b
Jeffrey North <JNorth@tql.com>
2021-06-06 15:49:06 -0400

    Add project files.

---
+++ TestSundayAgainAgainApi/Data/TestSundayAgainAgainApiContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestSundayAgainAgainApi.Models;
+
+namespace TestSundayAgainAgainApi.Data
+{
+    public class TestSundayAgainAgainApiContext : DbContext
+    {
+        public TestSundayAgainAgainApiContext (DbContextOptions<TestSundayAgainAgainApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestSundayAgainAgainApi.Models.Developer> Developer { get; set; }
+
+        public DbSet<TestSundayAgainAgainApi.Models.Assignment> Assignment { get; set; }
+
+        public DbSet<TestSundayAgainAgainApi.Models.Project> Project { get; set; }
+    }
+}
---
+++ TestSundayAgainAgainApi/Models/Assignment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestSundayAgainAgainApi.Models
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+
+        //FK
+        public virtual Developer Developer { get; set; }
+        public int DevloperId { get; set; }
+
+        public virtual Project Project { get; set; }
+        public int ProjectId { get; set; }
+
+        public Assignment() { }
+
+    }
+}
